3877a1588fc36e144ed594226bad53c6dbabc0e8
prasanthtootcat <prasanthtootcat@live.com>
2018-03-15 19:49:57 +0530

    Final commit: stemming & punctuation removal added... As of now bug free... Needs testing and documentation

--- wordAssociation.py
+++ wordAssociation.py
@@ -5,6 +5,9 @@ Created by prasanthtootcat
 
 import nltk,graph,collections
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
+from nltk.tokenize import RegexpTokenizer
 
 wordAssociation = []
 tokenizedInput = []
@@ -12,19 +15,24 @@ tokenizedCorpus = []
 stopWords=set(stopwords.words('english'))
 location=input("Enter the corpus file location\n")
 corpus = open(location, "r")
-tokenizedCorpusNotPreprocessed = nltk.tokenize.word_tokenize(corpus.read())
+punctuationRemovalRegex = RegexpTokenizer(r'\w+')
+tokenizedCorpusNotPreprocessed = punctuationRemovalRegex.tokenize(corpus.read())	#punctuations removed & corpus tokenized
 userInput = input("Enter the sentence to form Word Association Graph\n")
-tokenizedInputNotPreprocessed = nltk.tokenize.word_tokenize(userInput)
+tokenizedInputNotPreprocessed = punctuationRemovalRegex.tokenize(userInput)			#punctuations removed & userInput tokenized
+stemmer = PorterStemmer()
 
 for w in tokenizedCorpusNotPreprocessed:
 	wLower = w.lower()
-	if wLower not in stopWords:
-		tokenizedCorpus.append(wLower);
+	if wLower not in stopWords:							#Stop words removal from corpus
+		wLower = stemmer.stem(wLower)					#Stemming of corpus
+		tokenizedCorpus.append(wLower)
 
 for w in tokenizedInputNotPreprocessed:
 	wLower = w.lower()
-	if wLower not in stopWords:
-		tokenizedInput.append(wLower);
+	if wLower not in stopWords:							#Stop words from user input
+		wLower = stemmer.stem(wLower)					#Stemming of user input
+		tokenizedInput.append(wLower)
+
 
 inputLen = len(tokenizedInput)
 corpusLen = len(tokenizedCorpus)
